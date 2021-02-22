58c7df5bd2912c8781d29a47f14f6461a47a5635
derTino89 <t.liebenow@resko-gmbh.de>
2021-02-22 17:33:55 +0100

    Update polarPattern.py

--- polarPattern.py
+++ polarPattern.py
@@ -1,43 +1,41 @@
+# Very blunt tool to test 2D polar-pattern simulation:
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-rayAmount = 249
-sigX = np.array([])
-sigY = np.array([])
+# define a number of points (N) to segment the circle around a microphone in multiple points 'n':
+N = 249
 
 # creating a circle:
-k = np.linspace(0, 2, rayAmount)
-for n in k:
-    x = np.cos(n*2*np.pi)
+nx = np.array([])
+ny = np.array([])
+nPoints = np.zeros((N, 2))
+
+segments = np.linspace(0, 2, N)
+for n in segments:
+    x = np.cos(n*np.pi)
     if n < 1:
         y = np.sqrt(1 - (x*x))
     elif n >= 1:
         y = -np.sqrt(1 - (x*x))
-    sigX = np.append(sigX, x)
-    sigY = np.append(sigY, y)
-
-# prove it's a circle:
-# plt.plot(sigX[0:rayAmount], sigY[0:rayAmount])
-# plt.gca().set_aspect('equal', adjustable='box')
-# plt.xlabel("x")
-# plt.ylabel("y")
+    nx = np.append(nx, x)
+    ny = np.append(ny, y)
 
 # creating an array of 2D-vectors (dots around the circumference):
-rays = np.zeros((rayAmount, 2))
-for i in range(0, rayAmount):
-    vec = np.array([sigX[i], sigY[i]])
-    rays[i] = vec
+for i in range(0, N):
+    vec = np.array([nx[i], ny[i]])
+    nPoints[i] = vec
 
-# test, whether those dots are still a circle:
+# prove it's a circle:
 #plt.plot(*rays.T)
 #plt.gca().set_aspect('equal', adjustable='box')
 #plt.show()
 
 # copy the circle for every polar pattern:
-cardioid = rays.copy()
-biDir = rays.copy()
-omniDir = rays.copy()
-varDir = rays.copy()
+cardioid = nPoints.copy()
+biDir    = nPoints.copy()
+omniDir  = nPoints.copy()
+varDir   = nPoints.copy()
 
 # scaling / shaping of the circles with |(1-s) + s(rayDirection * micDirection)|:
 micDir = np.array([1, 0])
@@ -45,11 +43,20 @@ s_o = 0
 s_c = 0.5
 s_b = 1
 s_v = 0.65
-for n in range(0, rayAmount):
-    omniDir[n] *= abs(((1-s_o) + s_o * np.dot(omniDir[n], micDir)))
+for n in range(0, N):
+    omniDir[n]  *= abs(((1-s_o) + s_o * np.dot(omniDir[n], micDir)))
     cardioid[n] *= abs(((1-s_c) + s_c * np.dot(cardioid[n], micDir)))
-    biDir[n] *= abs(((1-s_b) + s_b * np.dot(biDir[n], micDir)))
-    varDir[n] *= abs(((1-s_v) + s_v * np.dot(varDir[n], micDir)))
+    biDir[n]    *= abs(((1-s_b) + s_b * np.dot(biDir[n], micDir)))
+    varDir[n]   *= abs(((1-s_v) + s_v * np.dot(varDir[n], micDir)))
+
+
+# plots:
+def render(signal):
+    plt.plot(*signal.T)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.minorticks_on()
+    plt.grid(True, linestyle='--')
+
 
 fig = plt.figure()
 ax = fig.gca()
@@ -58,17 +65,6 @@ ax.axvline(x=0, color='k', linewidth=.5)
 ax.set_xticks(np.arange(-1, 1.1, 0.2))
 ax.set_yticks(np.arange(-1, 1.1, 0.2))
 
-
-def render(signal):
-    plt.plot(*signal.T)
-    plt.gca().set_aspect('equal', adjustable='box')
-    plt.minorticks_on()
-    plt.grid(True, linestyle='--')
-
-    #plt.show()
-
-
-# plots:
 render(omniDir)
 render(cardioid)
 render(biDir)
